581a34bfb0c688c164307a1abec146dbd65a0d03
Yichao 'Peak' Ji <peakji@me.com>
2023-03-05 22:49:35 +0800

    test(choice): include logprobs in tests

--- tests/test_choice.py
+++ tests/test_choice.py
@@ -7,9 +7,32 @@ from basaran.choice import map_choice, reduce_choice
 class TestChoice:
     """Test functions for creating and merging choice objects."""
 
-    def test_merge(self):
+    def test_choice(self):
         """Test merging a list of choices into a single choice object."""
-        choice_a = map_choice("hello", 0)
-        choice_b = map_choice(" world", 0, finish_reason="stop")
-        merged = reduce_choice([choice_a, choice_b])
+        merged = reduce_choice(
+            [
+                map_choice(
+                    "hello",
+                    1,
+                    token=0,
+                    token_logprob=0,
+                    top_logprobs={},
+                    text_offset=0,
+                    finish_reason=None,
+                ),
+                map_choice(
+                    " world",
+                    1,
+                    token=1,
+                    token_logprob=0,
+                    top_logprobs={},
+                    text_offset=5,
+                    finish_reason="stop",
+                ),
+            ]
+        )
+
         assert merged["text"] == "hello world"
+        assert merged["index"] == 1
+        assert merged["finish_reason"] == "stop"
+        assert len(merged["logprobs"]["tokens"]) == 2
