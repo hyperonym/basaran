50521fd630b291a48c1ac6787f98cefa09b0980f
Gideon Giffard <118290024+fardeon@users.noreply.github.com>
2023-03-17 15:51:28 +0800

    build(utils): allow to download a specific revision (#53)

--- utils/download.py
+++ utils/download.py
@@ -14,12 +14,21 @@ if len(sys.argv) <= 1:
 # Get cache directory from arguments or environment variables.
 if len(sys.argv) >= 3:
     MODEL_CACHE_DIR = sys.argv[2]
-elif "MODEL_CACHE_DIR" in os.environ:
+elif "MODEL_CACHE_DIR" in os.environ and os.environ["MODEL_CACHE_DIR"]:
     MODEL_CACHE_DIR = os.environ["MODEL_CACHE_DIR"]
 else:
     MODEL_CACHE_DIR = None
 
+# Get model revision from environment variables.
+if "MODEL_REVISION" in os.environ and os.environ["MODEL_REVISION"]:
+    MODEL_REVISION = os.environ["MODEL_REVISION"]
+else:
+    MODEL_REVISION = None
+
 # Download a snapshot of the specified model from Hugging Face Hub.
 huggingface_hub.snapshot_download(
-    sys.argv[1], cache_dir=MODEL_CACHE_DIR, resume_download=True
+    sys.argv[1],
+    cache_dir=MODEL_CACHE_DIR,
+    revision=MODEL_REVISION,
+    resume_download=True,
 )
