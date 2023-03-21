c0d33e136a971c3d50f2da58f676049e770ac0eb
Yichao 'Peak' Ji <peakji@me.com>
2023-03-21 23:33:06 +0800

    build(ci): add workflow for publishing packages (#64)
    * build(ci): add workflow for publishing packages
    
    * build(ci): add docstring to setup.py
    
    * build(ci): sort elements

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+Script for building and distributing Python packages.
+"""
+from setuptools import find_packages, setup
+
+VERSION = "0.12.0"
+
+setup(
+    name="basaran",
+    version=VERSION,
+    description="Open-source alternative to the OpenAI text completion API",
+    author="Hyperonym",
+    author_email="prompt@hyperonym.org",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.8.0",
+    install_requires=["tenacity", "torch", "transformers"],
+    keywords=["api", "gpt", "huggingface", "nlp", "openai", "transformer"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
