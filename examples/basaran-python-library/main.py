3381fe7c432b0e0af393c8cc453f62042dbf52b7
Gideon Giffard <118290024+fardeon@users.noreply.github.com>
2023-03-22 17:56:10 +0800

    test(example): use real model in example to reduce ambiguity

--- examples/basaran-python-library/main.py
+++ examples/basaran-python-library/main.py
@@ -3,7 +3,7 @@ Example of using Basaran as a Python Library.
 """
 from basaran.model import load_model
 
-model = load_model("hf-internal-testing/tiny-random-t5")
+model = load_model("gpt2")
 
 for choice in model("once upon a time"):
     print(choice)
