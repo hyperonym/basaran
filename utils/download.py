8b6ab324c9f8ea60027471504ccd28970b5e8db4
Yichao 'Peak' Ji <peakji@me.com>
2023-03-29 15:13:04 +0800

    fix(utils): fix positional argument count

--- utils/download.py
+++ utils/download.py
@@ -8,14 +8,14 @@ import tempfile
 
 import huggingface_hub
 
-if len(sys.argv) <= 1:
+if len(sys.argv) < 3:
     sys.exit("usage: python download.py REPO_ID LOCAL_DIR [REVISION]")
 
 with tempfile.TemporaryDirectory() as cache_dir:
     huggingface_hub.snapshot_download(
         repo_id=sys.argv[1],
         local_dir=sys.argv[2],
-        revision=sys.argv[3] if len(sys.argv) >= 4 else None,
+        revision=sys.argv[3] if len(sys.argv) > 3 else None,
         cache_dir=cache_dir,
         local_dir_use_symlinks=False,
         resume_download=True,
