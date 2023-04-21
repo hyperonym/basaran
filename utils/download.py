d588be5c4e4e25dfa4c83539b5341f5d7054960b
Gideon Giffard <118290024+fardeon@users.noreply.github.com>
2023-04-21 10:28:47 +0800

    build(utils): allow to download safetensors format

--- utils/download.py
+++ utils/download.py
@@ -3,6 +3,7 @@ Use this script to download a model without loading it into memory.
 This allows memory-constrained CI/CD runners to build container images
 with large bundled models. See ../deployments/bundle/ for examples.
 """
+import os
 import sys
 import tempfile
 
@@ -10,6 +11,11 @@ import huggingface_hub
 
 if len(sys.argv) < 3:
     sys.exit("usage: python download.py REPO_ID LOCAL_DIR [REVISION]")
+
+if os.getenv("TENSOR_FORMAT") == "safetensors":
+    tensor_format = "*.safetensors"
+else:
+    tensor_format = "*.bin"
 
 with tempfile.TemporaryDirectory() as cache_dir:
     huggingface_hub.snapshot_download(
@@ -19,5 +25,5 @@ with tempfile.TemporaryDirectory() as cache_dir:
         cache_dir=cache_dir,
         local_dir_use_symlinks=False,
         resume_download=True,
-        allow_patterns=["*.bin", "*.json", "*.model", "*.py"],
+        allow_patterns=[tensor_format, "*.json", "*.model", "*.py"],
     )
